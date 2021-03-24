b6b87ef6a8b06ee4fa52aa14aad4748db38933b8
Renad-Ahmad <rena.34end@gmail.com>
2021-03-24 11:37:02 +0300

    add a function to take the student name and grade

---
+++ Students-grades-entry-system/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Students_grades_entry_system
+{
+    class Program
+    {
+        static void results(String name, int grade)
+        {
+            Console.WriteLine(name + " and his grade is " + grade);
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            var studentName = Console.ReadLine();
+            int studentGrade = Convert.ToInt32(Console.ReadLine());
+
+            results(studentName, studentGrade);
+        }
+
+
+    }
+}
