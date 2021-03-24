d66d61eea6c2692a212247e8bfd2d9be45009568
Renad-Ahmad <rena.34end@gmail.com>
2021-03-24 18:22:37 +0300

    Adding multiple user inputs

--- Students-grades-entry-system/Program.cs
+++ Students-grades-entry-system/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Students_grades_entry_system
 {
@@ -22,9 +21,9 @@ namespace Students_grades_entry_system
 
         static void Main(string[] args)
         {
-           
-            var studentName = Console.ReadLine();
-            int studentGrade = Convert.ToInt32(Console.ReadLine());
+
+            /*var studentName = Console.ReadLine();
+             int studentGrade = Convert.ToInt32(Console.ReadLine());
 
             List<string> ls = new List<string>();
             ls.Add(studentName +' '+ studentGrade);
@@ -33,8 +32,34 @@ namespace Students_grades_entry_system
             {
                 Console.WriteLine(item);
                 results(studentName, studentGrade);
+            }*/
+
+            List<string> numbersInput = new List<string>();
+
+            Console.WriteLine("Please enter an student Name and his Grade :");
+
+            string studentName = Console.ReadLine();
+            int studentGrade = Convert.ToInt32(Console.ReadLine());
+            var input = studentName +" "+ studentGrade;
+
+            numbersInput.Add(input);
+
+            while (input != "")
+            {
+                Console.WriteLine("Please enter another student: ");
+                input = Console.ReadLine();
+                numbersInput.Add(input);
             }
-            
+
+            if (input == "")
+            {
+                foreach (var item in numbersInput)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+
         }
     }
 }
